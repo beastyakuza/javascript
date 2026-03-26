d993180e76151bc05e1e1a868d92b3f8e8e77e51
sergio ivan fernandez galeano <sercolgi1@gmail.com>
2026-03-25 20:53:46 -0500

    de min a seg y visceversa

--- 0.2variables.js
+++ 0.2variables.js
@@ -1,8 +1,27 @@
-let lado = Number(prompt("ingrese la medida del lado del cuadrado"));
-let perimetro = lado * 4;
-console.log("el perimetro del cuadrado es" ,  perimetro)
-
-
+let tipo = prompt("en que vas a medir, en seg o en min")
+if(tipo == "min"){
+    let minutos = Number(prompt("ingrese los minutos"));
+    if(minutos <= 0){
+        console.log("no es numero valido para medir")
+    }
+    else{
+    let seg = minutos * 60
+    console.log("su medida en segundos es es" , seg)
+    }
+}
+else if(tipo == "seg"){
+     let seg = Number(prompt("ingrese el valor en seg"));
+     if(seg <= 0){
+        console.log("no es numero valido para medir")
+     }
+     else{
+     let minutos = seg / 60
+     console.log("su medida en minutos es" , minutos)
+     }
+}
+else{
+     console.log("el caracter no es correcto")
+}
 
 
 
